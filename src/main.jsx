6d3cb65a438f0fa9a38b70f3398e26dafaac7862
pranali <kokatepranu833@gmail.com>
2026-03-13 12:13:34 +0530

    Update main.jsx
    main page code added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Route,Routes } from 'react-router';
+import Home from './Views/Home';
+import { BrowserRouter, Routes,Route } from 'react-router';
 
 const root = createRoot(document.getElementById('root'));
 
   root.render(<BrowserRouter>
   <Routes>
-    <Route path ="/" element ={<h1>Home</h1>} />
+    <Route path ="/" element ={<Home />} />
 
   </Routes>
   </BrowserRouter>);
