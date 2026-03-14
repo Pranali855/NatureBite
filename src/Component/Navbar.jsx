2f53a46684c86603004841f0a783c7c36596fbe8
pranali <kokatepranu833@gmail.com>
2026-03-14 12:34:12 +0530

    Update Navbar.jsx
    navbar fixed properly

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 import { Salad as SaladIcon } from 'lucide-react';
 
 function Navbar() {
   return (
-    <nav className='bg-[#89986D] sticky top-5 w-7/14 mx-auto  gap-[-2] px-10 py-4 rounded-full flex items-center shadow-md'>
-      <SaladIcon className='w-8 h-8 text-white mr-4' />
-      <h4 className='text-3xl font-bold text-white'>NatureBite</h4>
+    <nav className="bg-[#89986D] sticky top-5 w-4/5 mx-auto px-8 py-4 rounded-full flex items-center shadow-md">
+      <SaladIcon className='w-10 h-10 text-white mr-4 flex-shrink-0' />
+      <h4 className="text-3xl font-bold text-white">NatureBite</h4>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
