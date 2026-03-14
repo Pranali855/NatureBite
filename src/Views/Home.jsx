e3a8d195b4c4f94f8c917189c54dd4531694dda1
pranali <kokatepranu833@gmail.com>
2026-03-14 13:38:58 +0530

    Update Home.jsx
    home page code added

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Navbar from '../Component/Navbar';
 import Footer from '../Component/Footer';
 import Img from './../assets/Feactures/bg-vegitable.png';
-import FeactureSection from '../Component/FeactureSection'; // Updated import
+import FeactureSection from '../Component/FeactureSection'; 
+import Button from './../Component/Button';
 
 function Home() {
   return (
@@ -10,20 +11,26 @@ function Home() {
       <Navbar />
 
       <div className="min-h-screen bg-[#F6F0D7] flex flex-col md:items-center items-center pt-6">
-        <div className="flex flex-col md:flex-row items-center justify-start gap-10 mt-10">
-          <div className="flex items-center">
-            <img 
-              src={Img} 
-              alt="Vegetable Image" 
-              className="h-[450px] w-auto object-contain" 
+        <div className="flex flex-col md:flex-row items-center justify-center gap-10 mt-10 flex-wrap">
+          <div className="flex items-center justify-center">
+            <img
+              src={Img}
+              alt="Vegetable Image"
+              className="h-[450px] w-auto object-contain"
             />
           </div>
-          <h1 className="text-[4rem] font-bold mt-8 text-[#AEB784] text-center md:text-[4rem]">
-            One platform for all your healthy food needs.
-          </h1>
+
+          <div className="flex flex-col items-center md:items-start text-center md:text-left max-w-lg">
+            <h1 className="text-[3rem] md:text-[4rem] font-bold text-[#AEB784]">
+              One platform for all your healthy food needs.
+            </h1>
+
+            <div className="mt-8">
+              <Button title="Call Us" />
+            </div>
+          </div>
         </div>
 
-        {/* Feature Section */}
         <FeactureSection />
 
       </div>
