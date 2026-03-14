457351c24e5665574fd3b87160067efd4419d91a
pranali <kokatepranu833@gmail.com>
2026-03-14 12:33:57 +0530

    Update Home.jsx
    changes added in home page

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -5,6 +5,7 @@ import Img from './../assets/Feactures/bg-vegitable.png';
 import IconFreshness from './../assets/Feactures/fresh.png';
 import IconVegitable from './../assets/Feactures/vegetable.png';
 import IconDelivery from './../assets/Feactures/food-delivery.png';
+import FeatureSection from '../Component/FeactureSection';
 
 const FEATURES_CONFIG = [
   {
@@ -29,37 +30,38 @@ function Home() {
     <div>
       <Navbar />
 
-      <div className="min-h-screen bg-[#F6F0D7] flex flex-col items-center pt-6">
+      <div className="min-h-screen bg-[#F6F0D7] flex-col md: item-center flex flex-col items-center pt-6">
         <div className="flex flex-col md:flex-row items-center justify-start gap-15 mt-10">
-          
+
           <div className="flex items-center">
-            <img src={Img} alt="Vegetable Image" className="h-[450px]" />
+            <img src={Img} alt="Vegetable Image" className="h-[450px] w-auto object-contain justify-center"  />
           </div>
 
-          <h1 className="text-[3rem] font-bold mt-8 text-[#AEB784] text-center">
+          <h1 className="text-[3rem] font-bold mt-8 text-[#AEB784] md: text[20px] text-center">
             One platform for all your healthy food needs.
           </h1>
 
         </div>
 
-        <h2 className="text-[2rem] text-[#C5D89D] mt-8  mr-6 text-center justify-center">
-          Why you should choose us?
-        </h2>
-        
+        <p className="text-[2rem] md:text-[1.5rem] text-[#AEB784] mt-[1.5rem]  nb-0 mb-[3rem] text-center justify-center">
+          Why You Should Choose Us
+        </p>
 
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-10">
           {FEATURES_CONFIG.map((feat, index) => {
             const { title, description, imgsrc } = feat;
-            return(
-            <img
-            key={index}
-            src={imgsrc}
-            alt={title}
-            className="h-[100px] mx-auto mt-10"
-           />
-      
+            return (
+              <FeatureSection 
+              key={index}
+              title={title}
+               description={description} 
+               imgsrc={imgsrc} 
+               />
             );
           })}
-</div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
