d8777bd1abce55afb7eb10c7f510e230b0e6a709
pranali <kokatepranu833@gmail.com>
2026-03-16 14:08:03 +0530

    Update Navbar.jsx
    changes added in navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,7 +4,9 @@ import { Salad as SaladIcon } from 'lucide-react';
 function Navbar() {
   return (
     <nav className="bg-[#89986D] sticky top-5 w-4/5 mx-auto px-8 py-4 rounded-full flex items-center shadow-md">
+      
       <SaladIcon className='w-10 h-10 text-white mr-4 flex-shrink-0' />
+      
       <h4 className="text-3xl font-bold text-white">NatureBite</h4>
     </nav>
   );
