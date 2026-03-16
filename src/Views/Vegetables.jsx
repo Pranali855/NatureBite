5ff253907ba607eb197aeb753a8be75dca22730d
pranali <kokatepranu833@gmail.com>
2026-03-16 14:07:11 +0530

    Update Vegetables.jsx
    vegitable page added

--- src/Views/Vegetables.jsx
+++ src/Views/Vegetables.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from '../Component/Navbar';
+import Footer from '../Component/Footer';
 
 function Vegetables() {
   return (
-    <div>Vegetables</div>
+    <div>
+      <Navbar/>
+      
+      <div className="min-h-screen bg-[#F6F0D7] flex flex-col md:items-center items-center pt-6">
+        {/* Add your vegetables content here */}
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
