1419d15b9daf899f91880362d694f4685ec19cb8
pranali <kokatepranu833@gmail.com>
2026-03-16 13:17:48 +0530

    Update Button.jsx
    button changes added

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-function Button({ title, onClick }) {
+const BUTTON_VARIANTS = {
+  primary: 'bg-[#AEB784] text-white hover:bg-[#9CAB84] gap-9px',
+  secondary: 'bg-[#AEB784] text-white hover:bg-[#9CAB84]',
+  Tertiary: 'bg-transparent border border-[#AEB784] text-[#AEB784] hover:bg-[#AEB784] hover:text-white',
+};
+
+const BUTTON_SIZES = {
+  large: "text-2xl",
+  medium: "text-xl",
+  small: "text-sm"
+};
+
+function Button({ title, onClick = () => {}, varient = 'primary', size = "medium" }) {
   return (
     <button
       onClick={onClick}
-      className="rounded-full bg-[#AEB784] text-white py-2  px-4 hover:bg-[#9CAB84]" 
+      className={`flex justify-start rounded-full ${BUTTON_VARIANTS[varient]} ${BUTTON_SIZES[size]} py-2 px-4 cursor-pointer mr-4`}
     >
       {title}
     </button>
